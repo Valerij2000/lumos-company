93e0ad5ad5c4769a9fd27fdc5d3a90e4722bfb97
Valerij <valery.shumkov@mail.ru>
2021-10-11 00:43:26 +0300

    create two block in html dev

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,8 @@
 $(function() {
-
-	// Пользовательские функции
-
+	$('a[href^="#"]').click(function(){ // #1
+		let anchor = $(this).attr('href');  // #2
+		$('html, body').animate({           // #3
+		scrollTop:  $(anchor).offset().top  // #4
+		});                            // #5
+		});
 });
